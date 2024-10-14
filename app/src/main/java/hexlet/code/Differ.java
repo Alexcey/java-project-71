2c957f72981aede5864a7940627129848f163883
alex <alexcey007@yandex.ru>
2024-10-14 14:17:05 +0300

    add file

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -8,6 +8,12 @@ public class Differ {
     private boolean helpRequested = false;
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     private boolean versionInfoRequested;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
     public static void generate() {
     }
 }
